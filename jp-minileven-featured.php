b0f91effbf15559f89b086f1199e88b21a2d756c
Evan Solomon <evan@evanalyze.com>
2012-12-13 23:41:48 -0800

    Make a few coding standard/style cleanups
    * Less indentation. Return early if conditionals fail instead of running most of your code inside of conditionals.
    * Don't check for adding the filter immediately, instead hook it to something (1) relatively late (2) front end only.
    * Simplify featured image detection with a ternary and remove some repetitive code.

--- jp-minileven-featured.php
+++ jp-minileven-featured.php
@@ -13,25 +13,23 @@
 // Check if we are on mobile
 // Props @saracannon http://ran.ge/2012/12/05/parallax-and-mobile/
 function tweakjp_is_mobile_or_tablet() {
-    if ( class_exists( 'Jetpack_User_Agent_Info' ) ) {
-        $ua_info = new Jetpack_User_Agent_Info();
-        return ( jetpack_is_mobile() || $ua_info->is_tablet() );
-    }
-    return false;
+    if ( ! class_exists( 'Jetpack_User_Agent_Info' ) )
+    	return false;
+
+    $ua_info = new Jetpack_User_Agent_Info();
+    return ( jetpack_is_mobile() || $ua_info->is_tablet() );
 }
 
 // On Mobile? Let's add the Featured Image
-if ( tweakjp_is_mobile_or_tablet() ) {
-	add_filter( 'the_title', 'tweakjp_minileven_featuredimage' );
+function tweakjp_maybe_add_filter() {
+	if ( tweakjp_is_mobile_or_tablet() )
+		add_filter( 'the_title', 'tweakjp_minileven_featuredimage' );
 }
-
+add_action( 'wp_head', 'tweakjp_maybe_add_filter' );
 
 function tweakjp_minileven_featuredimage( $title ) {
-	if ( has_post_thumbnail() && is_home() && in_the_loop() ) {
-	    $featured_content = get_the_post_thumbnail();
-	    $title .= $featured_content;
-	    return $title;
-	} else {
-		return $title;
-	}
+	$tweak = has_post_thumbnail() && is_home() && in_the_loop();
+	$featured_content = ( $tweak ) ? get_the_post_thumbnail() : '';
+
+	return $title . $featured_content;
 }
